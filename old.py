47ae74b835de5f206dad2a124b1b616cd6d61b5e
alexamarki <amarkin2006@gmail.com>
2023-01-18 01:09:34 +0300

    Added gradual speed increase + scoring system, the max speed limit bug still has to be figured out though

--- old.py
+++ old.py
@@ -1,28 +1,33 @@
 import pygame
 import random
+
 pygame.font.init()
 blockScale = 24
-gameWidth, gameHeight = 40, 40
+gameWidth, gameHeight = 10, 20
 (width, height) = (60 * blockScale, 40 * blockScale)
 all_sprites = pygame.sprite.Group()
 current_tetromino = []
-tickSpeed = 300
-tempTick = 300
+tickSpeed = (1/3) * 1000
+tempTick = tickSpeed
 boardCentreX = 20
 boardCentreY = 20
 boardTopX = boardCentreX - gameWidth / 2
 boardTopY = boardCentreY - gameHeight / 2
 rotor = 1
+score = 0
 
 classicBase = [['' for x in range(gameWidth)] for y in range(gameHeight)]
 holdContainer = ''
 nextLetter = ''
+holdPause = False
+
 
 class randomiser():
     def randomiseletter(self):
         global nextLetter
-        nextLetter= random.choice(['j', 'l', 'o', 'i', 's', 'z', 't'])
+        nextLetter = random.choice(['j', 'l', 'o', 'i', 's', 'z', 't'])
         return nextLetter
+
     def defineSpawn(self, keyer=''):
         if not keyer:
             keyer = nextLetter
@@ -32,39 +37,44 @@ class randomiser():
         else:
             return keyer, (gameWidth - 4) // 2 + 1
 
+
 class infoBlock():
     def displayTestTetros(self, letter, xdisc, ydisc):
         x1, y1 = (tetrominoDisplay().coords.get(letter)[0])
         x2, y2 = (tetrominoDisplay().coords.get(letter)[1])
         x3, y3 = (tetrominoDisplay().coords.get(letter)[2])
         x4, y4 = (tetrominoDisplay().coords.get(letter)[3])
-        tetrominoBlock(x1 + xdisc, y1 + ydisc, letter + 'mino.png').update()
-        tetrominoBlock(x2 + xdisc, y2 + ydisc, letter + 'mino.png').update()
-        tetrominoBlock(x3 + xdisc, y3 + ydisc, letter + 'mino.png').update()
-        tetrominoBlock(x4 + xdisc, y4 + ydisc, letter + 'mino.png').update()
+        tetrominoBlock(x1 + xdisc, y1 + ydisc, letter + 'mino.png', False).update()
+        tetrominoBlock(x2 + xdisc, y2 + ydisc, letter + 'mino.png', False).update()
+        tetrominoBlock(x3 + xdisc, y3 + ydisc, letter + 'mino.png', False).update()
+        tetrominoBlock(x4 + xdisc, y4 + ydisc, letter + 'mino.png', False).update()
 
 
 class tetrominoBlock(pygame.sprite.Sprite):
     def __init__(self, x, y, filename, screen=True):
         super().__init__(all_sprites)
-        self.x = x + boardTopX
-        self.y = y + boardTopY
+        self.x = x
+        self.y = y
         self.image = pygame.image.load(filename).convert_alpha()
         self.rect = self.image.get_rect()
         if screen:
+            self.x += boardTopX
+            self.y += boardTopY
             self.rotationSit()
 
     def rotationSit(self):
         if rotor == 1:
             pass
         elif rotor == 2:
-            self.x, self.y = boardCentreX + boardCentreY - self.y, -boardCentreX + boardCentreY + self.x
+            self.x, self.y = boardCentreX + boardCentreY - self.y - 1, -boardCentreX + boardCentreY + self.x
         elif rotor == 3:
-            self.x, self.y = boardCentreX * 2 - self.x, boardCentreY * 2 - self.y
+            self.x, self.y = boardCentreX * 2 - self.x - 1, boardCentreY * 2 - self.y - 1
         else:
-            self.x, self.y = boardCentreX - boardCentreY + self.y, boardCentreX + boardCentreY - self.x
+            self.x, self.y = boardCentreX - boardCentreY + self.y, boardCentreX + boardCentreY - self.x - 1
+
     def update(self):
-        screen.blit(self.image, ((self.x)*blockScale, (self.y)*blockScale))
+        screen.blit(self.image, ((self.x) * blockScale, (self.y) * blockScale))
+
 
 class tableHandler():
     def convertToFallen(self):
@@ -73,27 +83,43 @@ class tableHandler():
         current_tetromino.clear()
         global tickSpeed
         tickSpeed = 300
+        global holdPause
+        holdPause = False
+
     def lineEraser(self):
+        score = 0
+        count = 0
         for i in range(gameHeight):
             if '' not in classicBase[i]:
-                for it in range(gameWidth ):
+                count += 1
+                for it in range(gameWidth):
                     classicBase[i][it] = ''
                 for j in range(i, gameWidth - 1, -1):
-                    for l in range(gameWidth ):
-                        if classicBase[j-1][l].islower():
-                            item = classicBase[j-1][l]
-                            classicBase[j-1][l] = ''
+                    for l in range(gameWidth):
+                        if classicBase[j - 1][l].islower():
+                            item = classicBase[j - 1][l]
+                            classicBase[j - 1][l] = ''
                             classicBase[j][l] = item
+        if count == 1:
+            score = 40
+        elif count == 2:
+            score = 100
+        elif count == 3:
+            score = 200
+        elif score == 4:
+            score = 300
+        return score
 
 
 class screenRefresh():
     def refresh(self):
-            all_sprites.empty()
-            for y in range(gameHeight):
-                for x in range(gameWidth):
-                    # if classicBase[y][x] != '':
-                        block = tetrominoBlock(x, y, classicBase[y][x].upper() + 'mino.png')
-                        block.update()
+        all_sprites.empty()
+        for y in range(gameHeight):
+            for x in range(gameWidth):
+                # if classicBase[y][x] != '':
+                block = tetrominoBlock(x, y, classicBase[y][x].upper() + 'mino.png')
+                block.update()
+
 
 class tetrominoDisplay():
     def __init__(self, x=0, y=0, letter=''):
@@ -127,17 +153,18 @@ class tetrominoDisplay():
     def movementCheck(self, yMod, xMod):
         allowed = True
         for i in current_tetromino:
-            if i[1] + yMod >= gameHeight or i[0] + xMod >= gameWidth  or i[0] - xMod < 0:
+            if i[1] + yMod >= gameHeight or i[0] + xMod >= gameWidth or i[0] - xMod < 0:
                 allowed = False
         if allowed:
             for i in current_tetromino:
-                if classicBase[i[1] + yMod][i[0] + xMod] in ('j','l','i','o','s','z','t'):
+                if classicBase[i[1] + yMod][i[0] + xMod] in ('j', 'l', 'i', 'o', 's', 'z', 't'):
                     allowed = False
         if allowed:
             self.move(yMod, xMod)
         elif not allowed and not xMod:
             tableHandler().convertToFallen()
-            tableHandler().lineEraser()
+            return tableHandler().lineEraser()
+        return 0
 
     def move(self, yMod, xMod):
         a = []
@@ -149,22 +176,24 @@ class tetrominoDisplay():
         n = 0
         for i in current_tetromino:
             classicBase[i[1]][i[0]] = a[n]
-            n+=1
+            n += 1
 
     def rotationCheck(self, n):
         allowed = True
         for i in current_tetromino:
-            if i[0] - self.x + self.y >= gameHeight or (n - 1) - (i[1] - self.y) + self.x >= gameWidth or (n - 1) - (i[1] - self.y) + self.x < 0:
+            if i[0] - self.x + self.y >= gameHeight or (n - 1) - (i[1] - self.y) + self.x >= gameWidth or (n - 1) - (
+                    i[1] - self.y) + self.x < 0:
                 allowed = False
         if allowed:
             for i in current_tetromino:
-                if classicBase[i[0] - self.x + self.y][(n - 1) - (i[1] - self.y) + self.x] in ('j','l','i','o','s','z','t'):
+                if classicBase[i[0] - self.x + self.y][(n - 1) - (i[1] - self.y) + self.x] in (
+                'j', 'l', 'i', 'o', 's', 'z', 't'):
                     allowed = False
         if allowed:
             self.rotate(n)
 
     def rotate(self, n):
-        a=[]
+        a = []
         for i in current_tetromino:
             a.append(classicBase[i[1]][i[0]])
             classicBase[i[1]][i[0]] = ''
@@ -174,6 +203,7 @@ class tetrominoDisplay():
             classicBase[i[1]][i[0]] = a[n]
             n += 1
         screenRefresh().refresh()
+
 
 nextLetter = randomiser().randomiseletter()
 font = pygame.font.SysFont('boldTestFont.ttf', 24)
@@ -200,6 +230,8 @@ elapsedT = 0
 timeClock = pygame.time.Clock()
 movementStop = False
 holdPause = False
+
+
 class holding():
     def holder(self):
         if not holdContainer:
@@ -214,7 +246,36 @@ class holding():
         current_tetromino.clear()
         tetrominoDisplay(x, y, keyed).display()
         return keyed, xCorner, yCorner, movementStop
+
+
+class keyMovement():
+    def moveDown(self):
+        pass
+
+    def moveRight(self):
+        pass
+
+    def moveLeft(self):
+        pass
+
+    def moveDownQuick(self):
+        pass
+
+fullClock = 0
+stage = 0
 while running:
+    stage = int(fullClock / 50000)
+    if tempTick > (1/(23)) * 1000:
+        if tickSpeed != 5:
+            tickSpeed = (1/(3 + stage)) * 1000
+            tempTick = tickSpeed
+            print(stage, tickSpeed, tempTick)
+        else:
+            tempTick = (1/(3 + stage)) * 1000
+            print(stage, tickSpeed, tempTick)
+    imgScore = font.render(str(score), True, (0, 23, 43))
+    rect = imgScore.get_rect()
+    pygame.draw.rect(img, (255, 255, 255), rect, 1)
     for i in current_tetromino:
         if i[1] == gameHeight - 1:
             movementStop, movementPauseL, movementPauseR, rotLock = True, True, True, True
@@ -223,9 +284,10 @@ while running:
         if i[0] == 0:
             movementPauseL = True
     dt = timeClock.tick()
+    fullClock += dt
     elapsedT += dt
     if elapsedT > tickSpeed and not movementStop:
-        tetrominoDisplay().movementCheck(1, 0)
+        score += tetrominoDisplay().movementCheck(1, 0) * (1 + stage)
         screenRefresh().refresh()
         yCorner += 1
         elapsedT = 0
@@ -234,16 +296,16 @@ while running:
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT and not movementPauseL:
-                tetrominoDisplay().movementCheck(0, -1)
+                score += tetrominoDisplay().movementCheck(0, -1) * (1 + stage)
                 screenRefresh().refresh()
                 xCorner -= 1
             elif event.key == pygame.K_RIGHT and not movementPauseR:
-                tetrominoDisplay().movementCheck(0, 1)
+                score += tetrominoDisplay().movementCheck(0, 1) * (1 + stage)
                 screenRefresh().refresh()
                 xCorner += 1
             elif event.key == pygame.K_DOWN:
                 tempTick = tickSpeed
-                tickSpeed = gameWidth 
+                tickSpeed = 5
             elif event.key == pygame.K_UP and rotator and not rotLock:
                 if rotator == 'i':
                     tetrominoDisplay(xCorner, yCorner, rotator).rotationCheck(4)
@@ -271,17 +333,19 @@ while running:
         tetrominoDisplay(x, y, keyed).display()
         rotator, keyed = keyed, ''
     if not movementStop:
-        movementPauseL, movementPauseR, rotLock, holdPause = False, False, False, False
+        movementPauseL, movementPauseR, rotLock = False, False, False
     else:
         tableHandler().convertToFallen()
         tickSpeed = tempTick
-        tableHandler().lineEraser()
+        score += tableHandler().lineEraser() * (1 + stage)
+        holdPause = False
     screen.fill((255, 255, 255))
     screenRefresh().refresh()
     screen.blit(img, (50 * blockScale, 5 * blockScale))
     screen.blit(img2, (50 * blockScale, 10 * blockScale))
     screen.blit(img3, (50 * blockScale, 15 * blockScale))
+    screen.blit(imgScore, (50 * blockScale, 7 * blockScale))
     if holdContainer:
-        infoBlock().displayTestTetros(holdContainer, 35, 8)
-    infoBlock().displayTestTetros(nextLetter, 35, 0)
+        infoBlock().displayTestTetros(holdContainer, 50, 17)
+    infoBlock().displayTestTetros(nextLetter, 50, 12)
     pygame.display.flip()
